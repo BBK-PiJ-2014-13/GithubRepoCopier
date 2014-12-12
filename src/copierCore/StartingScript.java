9cb3ced2e63238f48c31184fd5a24cd3208d7047
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-12 22:46:00 +0000

    Cosmetic changes

--- src/copierCore/StartingScript.java
+++ src/copierCore/StartingScript.java
@@ -3,10 +3,10 @@ package copierCore;
 import com.jcabi.github.Repo;
 
 public class StartingScript {
-public static void main(String[] args) {
-	Copier copier = new CopierImpl();
-	Repo repo = copier.getRepo("BBK-PiJ-2014-13/cw3");
-	System.out.println("Repository name is: " + repo.coordinates().repo());
-	System.out.println("Repository user is: " + repo.coordinates().user());
-}
+	public static void main(String[] args) {
+		Copier copier = new CopierImpl();
+		Repo repo = copier.getRepo("BBK-PiJ-2014-13/cw3");
+		System.out.println("Repository name is: " + repo.coordinates().repo());
+		System.out.println("Repository user is: " + repo.coordinates().user());
+	}
 }
