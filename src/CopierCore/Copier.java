df42701e539d0c2359ae021186cc10ddb133b4cc
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-12 22:09:36 +0000

    Added comments

--- src/CopierCore/Copier.java
+++ src/CopierCore/Copier.java
@@ -6,6 +6,8 @@ import com.jcabi.github.Repo;
 /**
  * Copies repositories from Github and returns them wrapped in an object
  * 
+ * Stores repository address as a String
+ * 
  * @author Ilya Ivanov
  */
 public interface Copier {
