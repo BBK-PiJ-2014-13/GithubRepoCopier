20eefaabfc927cce7d987f3f04184f6377c6c91b
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-12 22:16:44 +0000

    Copied class BasicTest

---
+++ src/copierCore/BasicTest.java
@@ -0,0 +1,16 @@
+package copierCore;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class BasicTest {
+	Object valueExpected;
+	Object valueActual;
+	
+	@Test
+	public void test() {
+		assertEquals(valueExpected, valueActual);
+	}
+}
