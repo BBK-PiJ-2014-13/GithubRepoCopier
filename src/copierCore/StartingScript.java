349bee65889603b870cf174f5eb149a844f6d3bd
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-12 22:37:34 +0000

    Added main method to StartingScript

--- src/copierCore/StartingScript.java
+++ src/copierCore/StartingScript.java
@@ -1,5 +1,12 @@
 package copierCore;
 
-public class StartingScript {
+import com.jcabi.github.Repo;
 
+public class StartingScript {
+public static void main(String[] args) {
+	Copier copier = new CopierImpl();
+	Repo repo = copier.getRepo("BBK-PiJ-2014-13/cw3");
+	System.out.println("Repository name is: " + repo.coordinates().repo());
+	System.out.println("Repository user is: " + repo.coordinates().user());
+}
 }
