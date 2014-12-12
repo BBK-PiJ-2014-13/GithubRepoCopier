0e42cc8c876548bb35250cd4de55a040f4b2a6c0
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-12 22:31:09 +0000

    Added variables github and repo

--- src/copierCore/CopierImpl.java
+++ src/copierCore/CopierImpl.java
@@ -1,12 +1,16 @@
 package copierCore;
 
+import com.jcabi.github.Coordinates;
+import com.jcabi.github.Github;
 import com.jcabi.github.Repo;
+import com.jcabi.github.RtGithub;
 
 public class CopierImpl implements Copier{
 	
 	@Override
 	public Repo getRepo(String s) {
-		
+		Github github = new RtGithub();
+		Repo repo = github.repos().get(new Coordinates.Simple("BBK-PiJ-2014-13/cw3"));
 		return repo;
 	}
 
