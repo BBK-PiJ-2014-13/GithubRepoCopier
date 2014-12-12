1bdf28d09c4b5a232272d6911c1d5f2587b6f576
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-12 22:08:26 +0000

    Added method getRepo()

--- src/CopierCore/Copier.java
+++ src/CopierCore/Copier.java
@@ -1,9 +1,16 @@
+
 package CopierCore;
 
+import com.jcabi.github.Repo;
+
 /**
+ * Copies repositories from Github and returns them wrapped in an object
+ * 
  * @author Ilya Ivanov
- *
  */
 public interface Copier {
-
+	/**
+	 * @return the requested repository
+	 */
+	public Repo getRepo();
 }
