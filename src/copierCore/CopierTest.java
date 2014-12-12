f1faab5935f53d8abbd8bfb095e644b09c3f3d96
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-12 22:21:20 +0000

    Added body to the method

--- src/copierCore/CopierTest.java
+++ src/copierCore/CopierTest.java
@@ -2,10 +2,17 @@ package copierCore;
 
 import org.junit.Test;
 
+import com.jcabi.github.Coordinates;
+import com.jcabi.github.Github;
+import com.jcabi.github.Repo;
+import com.jcabi.github.RtGithub;
+
 public class CopierTest extends BasicTest {
-	
+
 	@Test
 	public void testsGetRepo() {
-
+		Github github = new RtGithub();
+		Repo repo = github.repos().get(new Coordinates.Simple("BBK-PiJ-2014-13/cw3"));
+		test();
 	}
 }
