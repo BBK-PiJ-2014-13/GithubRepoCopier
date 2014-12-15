a355912b8be60189d1a048fc7501b8e0ead5a7cc
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-15 22:47:27 +0000

    Wrote test for getIterator()

--- src/copierCore/CopierTest.java
+++ src/copierCore/CopierTest.java
@@ -1,7 +1,10 @@
 package copierCore;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
+import com.jcabi.github.Contents;
 import com.jcabi.github.Coordinates;
 import com.jcabi.github.Github;
 import com.jcabi.github.Repo;
@@ -43,7 +46,14 @@ public class CopierTest extends BasicTest {
 	}
 	
 	@Test
-	public void testsGetIterator() {
+	public void testsGetIterator() throws IOException {
+		copier = new CopierImpl();
+		github = new RtGithub();
+		repo = github.repos().get(new Coordinates.Simple("BBK-PiJ-2014-13/Test"));
+		Contents contents = repo.contents();
+		valueActual = copier.getIterator(contents, "", "master");
 		
+		valueExpected = repo.contents().iterate("", "master");
+		test();
 	}
 }
