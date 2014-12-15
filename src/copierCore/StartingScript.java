aa1c7f91af37be278a604415b916883b590ff9e0
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-15 10:40:24 +0000

    Removed print statements

--- src/copierCore/StartingScript.java
+++ src/copierCore/StartingScript.java
@@ -6,7 +6,5 @@ public class StartingScript {
 	public static void main(String[] args) {
 		Copier copier = new CopierImpl();
 		Repo repo = copier.getRepo("BBK-PiJ-2014-13/cw3");
-		System.out.println("Repository name is: " + repo.coordinates().repo());
-		System.out.println("Repository user is: " + repo.coordinates().user());
 	}
 }
