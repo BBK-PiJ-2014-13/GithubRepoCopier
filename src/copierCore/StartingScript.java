f17813f3b2fbe0df82b64fbc72b1e8e888387b45
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-16 19:36:07 +0000

    Changed repository to TestCopier

--- src/copierCore/StartingScript.java
+++ src/copierCore/StartingScript.java
@@ -5,6 +5,6 @@ import com.jcabi.github.Repo;
 public class StartingScript {
 	public static void main(String[] args) {
 		Copier copier = new CopierImpl();
-		Repo repo = copier.getRepo("BBK-PiJ-2014-13/test");
+		copier.launch("BBK-PiJ-2014-13/TestCopier");
 	}
 }
